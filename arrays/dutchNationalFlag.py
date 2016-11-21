1d4248bcb5bfd0ca97c1257207313c58395f1b9b
Santosh Gogi <santoshag@gmail.com>
2016-11-21 12:15:23 -0800

    formatting

--- arrays/dutchNationalFlag.py
+++ arrays/dutchNationalFlag.py
@@ -1,6 +1,6 @@
 def partitionDNF(A, pivot):
-    smaller,equal = 0, 0
-    larger = len(A) -1
+    smaller, equal = 0, 0
+    larger = len(A) - 1
     while equal < larger:
         print smaller, equal, larger, A[equal]
         if A[equal] < pivot:
@@ -13,7 +13,7 @@ def partitionDNF(A, pivot):
             A[larger], A[equal] = A[equal], A[larger]
             larger -= 1
 
-A = [4, 5, 6, 5, 6, 6,7,8,8, 9,10,9,10,1,3]
+A = [4, 5, 6, 5, 6, 6, 7, 8, 8, 9, 10, 9, 10, 1, 3]
 pivot = 10
 print 'Before partition:', A, 'Pivot:', pivot
 partitionDNF(A, pivot)
